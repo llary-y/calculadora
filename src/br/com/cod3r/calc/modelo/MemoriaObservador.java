539712315375939929446bb50eb5d789fa44ffae
Laryssa Silva <141759410+llary-y@users.noreply.github.com>
2023-09-18 15:36:03 -0300

    Add files via upload

---
+++ src/br/com/cod3r/calc/modelo/MemoriaObservador.java
@@ -0,0 +1,8 @@
+package br.com.cod3r.calc.modelo;
+
+public interface MemoriaObservador {
+
+	void valorAlterado (String novoValor);
+		
+	
+}
---
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -0,0 +1,35 @@
+package br.com.cod3r.calc.visao;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import br.com.cod3r.calc.modelo.Memoria;
+import br.com.cod3r.calc.modelo.MemoriaObservador;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel implements MemoriaObservador{
+
+	private final JLabel label;
+	
+	public Display() {
+		Memoria.getInstancia().adicionarObservador(this);
+		setBackground(new Color(75, 75, 76, 255)); // cor por RGBA
+		label = new JLabel(Memoria.getInstancia().getTextoAtual());
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Liberation Sans", Font.PLAIN, 25)); // nome, estilo, tamanho
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10,25));
+		
+		add(label);
+	}
+	
+	@Override
+	public void valorAlterado(String novoValor) {
+		label.setText(novoValor);
+		
+	}
+}
